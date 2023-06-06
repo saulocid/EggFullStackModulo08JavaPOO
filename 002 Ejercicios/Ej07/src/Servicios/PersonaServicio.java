fc8fff649c2254732113762f6230ece44424db2c
Saulo Cid <saulodavidcid@gmail.com>
2023-06-05 22:29:36 -0300

    modifico ejercicio 7

--- 002 Ejercicios/Ej07/src/Servicios/PersonaServicio.java
+++ 002 Ejercicios/Ej07/src/Servicios/PersonaServicio.java
@@ -50,11 +50,13 @@ public class PersonaServicio {
         per.setNombre(leer.next());
         System.out.print("Ingrese la edad: ");
         per.setEdad(leer.nextInt());
-        do{
-        System.out.print("Ingrese el sexo M, F u O: ");
-        per.setSexo(leer.next().charAt(0));
-        if(per.getSexo()!='M' && per.getSexo()!='F' && per.getSexo()!='O') System.out.println("Sexo incorrecto");
-        }while(per.getSexo()!='M' && per.getSexo()!='F' && per.getSexo()!='O');
+        do {
+            System.out.print("Ingrese el sexo M, F u O: ");
+            per.setSexo(leer.next().charAt(0));
+            if (per.getSexo() != 'M' && per.getSexo() != 'F' && per.getSexo() != 'O') {
+                System.out.println("Sexo incorrecto");
+            }
+        } while (per.getSexo() != 'M' && per.getSexo() != 'F' && per.getSexo() != 'O');
         System.out.print("Ingrese el peso: ");
         per.setPeso(leer.nextFloat());
         System.out.print("Ingrese la altura: ");
@@ -63,25 +65,39 @@ public class PersonaServicio {
 
         return per;
     }
-    
-    public int calcularIMC(Persona per){
-        int valor=0;
-        
+
+    public int calcularIMC(Persona per) {
+        int valor = 0;
+        int imc = (int) (per.getPeso() / (per.getAltura() * per.getAltura()));
+
         System.out.println("Calculando el IMC");
-        if(per.getPeso()/(per.getAltura()*(per.getAltura()))<20){
+        if (imc < 20) {
             valor = -1;
-        } else if (per.getPeso()/(per.getAltura()*(per.getAltura()))>25){
+        } else if (imc > 25) {
             valor = 1;
         }
-        
-        System.out.println("Su IMC es " + per.getPeso()/(per.getAltura()*(per.getAltura())));
-        
+
+        System.out.println("Su IMC es " + imc);
+
         return valor;
     }
+
+    public void esMayorDeEdad(Persona per, int[] edad) {
+        if (per.getEdad() < 18) {
+            System.out.println("Eres menor de edad");
+            edad[0]++;
+        } else {
+            System.out.println("Eres mayor de edad");
+            edad[1]++;
+        }
+    }
     
-    public void  esMayorDeEdad(Persona per){
-        if(per.getEdad()<18) System.out.println("Eres menor de edad");
-        else System.out.println("Eres mayor de edad");
+    public void mostrarPorcentajes(int[] contEdad, int[] contImc){
+        System.out.println("Porcentaje mayor de edad " + (contEdad[1] * 100 / 4) + "%");
+        System.out.println("Porcentaje menor de edad " + (contEdad[0] * 100 / 4) + "%");
+        System.out.println("Porcentaje debajo del peso ideal " + (contImc[0] * 100 / 4) + "%");
+        System.out.println("Porcentaje PESO IDEAL " + (contImc[1] * 100 / 4) + "%");
+        System.out.println("Porcentaje encima del peso ideal " + (contImc[2] * 100 / 4) + "%");
     }
 
 }
--- 002 Ejercicios/Ej07/src/ej07/Ej07.java
+++ 002 Ejercicios/Ej07/src/ej07/Ej07.java
@@ -6,49 +6,51 @@ import Servicios.PersonaServicio;
 public class Ej07 {
 
     public static void main(String[] args) {
-       
+
         PersonaServicio ps = new PersonaServicio();
-        
-        Persona p1 = new Persona();
-        Persona p2 = new Persona();
-        Persona p3 = new Persona();
-        Persona p4 = new Persona();
-        
-        Persona[] persona = {p1, p2, p3, p4};
-        
+
+        Persona[] persona = new Persona[4];
+        int[] contImc = {0, 0, 0};
+        int[] contEdad = {0, 0};
+
         System.out.println("");
         for (int i = 0; i < persona.length; i++) {
-            System.out.println("Ingrese datos para la persona Nº " + (i+1));
-            persona[i]=ps.crearPersona();
+            System.out.println("Ingrese datos para la persona Nº " + (i + 1));
+            persona[i] = ps.crearPersona();
         }
         System.out.println("");
-        
+
         for (int i = 0; i < persona.length; i++) {
-            System.out.println("Datos persona Nº " + (i+1));
-            validacion(persona[i], ps);
+            System.out.println("Datos persona Nº " + (i + 1));
+            validacion(persona[i], ps, contImc);
+            ps.esMayorDeEdad(persona[i], contEdad);
             System.out.println("");
         }
 
-    }
-    
-    public static void validacion(Persona per, PersonaServicio ps){
+        ps.mostrarPorcentajes(contEdad, contImc);
         
+
+    }
+
+    public static void validacion(Persona per, PersonaServicio ps, int[] contadorImc) {
+
         int valor;
         valor = ps.calcularIMC(per);
-        
-        switch(valor){
+
+        switch (valor) {
             case -1:
+                contadorImc[0]++;
                 System.out.println("Estás por debajo del peso ideal");
                 break;
             case 1:
+                contadorImc[2]++;
                 System.out.println("Estás por encima del peso ideal");
                 break;
             default:
+                contadorImc[1]++;
                 System.out.println("ESTÁS EN EL PESO IDEAL!!!");
         }
-        
-        ps.esMayorDeEdad(per);
-        
+
     }
 
 }
